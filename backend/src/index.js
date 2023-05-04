f9e6b89bbb76323a5ff3ea3a0fe2f334c3fe3ca0
daichi0918 <kayasugadaichi@icloud.com>
2023-05-04 15:05:21 +0900

    create backend

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fastify_1 = __importDefault(require("fastify"));
+const server = (0, fastify_1.default)({
+    logger: true,
+});
+server.get('/', (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
+    reply.type('application/json').code(200);
+    return { hello: 'world' };
+}));
+server.listen(3000, (err, address) => {
+    if (err)
+        throw err;
+    server.log.info(`server listening on ${address}`);
+});
